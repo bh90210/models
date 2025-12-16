72be27c71f47c9cde35d7434248550aa0e0bab18
bh90210 <ktc@protonmail.ch>
2025-12-16 22:16:02 +0200

    Nymphes

--- examples/nymphes/main.go
+++ examples/nymphes/main.go
@@ -2,71 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 
-	"gitlab.com/gomidi/midi/writer"
-	driver "gitlab.com/gomidi/rtmididrv"
+	"github.com/bh90210/models/nymphes"
 )
 
 func main() {
-	err := midiConnection()
+	nymphesSynth, err := nymphes.NewProject()
 	if err != nil {
 		panic(err)
 	}
+	defer nymphesSynth.Close()
 
-	select {}
-}
-
-func midiConnection() error {
-	drv, err := driver.New()
-	if err != nil {
-		return err
-	}
-
-	outs, _ := drv.Outs()
-	for i, out := range outs {
-		// if strings.Contains(out.String(), string(m)) {
-		// }
-		fmt.Println(out)
-
-		if i == 1 {
-			fmt.Println(out, i)
-			err = out.Open()
-			if err != nil {
-				return err
-			}
-
-			wr := writer.New(out)
-
-			wr.SetChannel(0)
-
-			for j := 0; j < 5; j++ {
-				writer.NoteOn(wr, 50, 120)
-				time.Sleep(1500 * time.Millisecond)
-				writer.NoteOff(wr, 50)
-
-				writer.NoteOn(wr, 60, 120)
-				time.Sleep(1500 * time.Millisecond)
-				writer.NoteOff(wr, 60)
-
-				writer.NoteOn(wr, 70, 120)
-				time.Sleep(1500 * time.Millisecond)
-				writer.NoteOff(wr, 70)
-
-				writer.NoteOn(wr, 75, 120)
-				time.Sleep(1500 * time.Millisecond)
-				writer.NoteOff(wr, 75)
-
-				fmt.Println(j)
-			}
-			// writer.ControlChange(wr, 20, 20)
+	in := nymphesSynth.Incoming()
+	go func() {
+		for {
+			val := <-in
+			println("MIDI IN:", val)
 		}
-	}
+	}()
 
-	err = p.out.Open()
-	if err != nil {
-		return nil, err
-	}
+	for i := range 5 {
+		nymphesSynth.Note(0, 50, 120, 1500)
+		nymphesSynth.Note(0, 60, 120, 1500)
+		nymphesSynth.Note(0, 70, 120, 1500)
+		nymphesSynth.Note(0, 75, 120, 1500)
 
-	return nil
+		fmt.Println(i)
+	}
 }
