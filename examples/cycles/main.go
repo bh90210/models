46e2d347ad8e6815df22e85f55fece0cbba435c4
bh90210 <ktc@protonmail.ch>
2026-01-07 02:04:49 +0200

    Cycles demo

--- examples/cycles/main.go
+++ examples/cycles/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/bh90210/models/cycles"
@@ -10,6 +11,9 @@ import (
 )
 
 func main() {
+	startFrom := flag.Int("position", 0, "start the audio playback form the designated pattern position.")
+	flag.Parse()
+
 	p, err := m.NewProject(m.CYCLES)
 	if err != nil {
 		panic(err)
@@ -26,12 +30,37 @@ func main() {
 
 	allVoices := make(map[int][]pattern.Pattern)
 
-	note := pattern.Note{Note: midicom.Note(cycles.C4), Duration: 200, Velocity: 100}
+	compo(allVoices, p)
 
+	// Trim patterns to start from designated position.
+	trimmedVoices := make(map[int][]pattern.Pattern)
+	for k, v := range allVoices {
+		trimmedVoices[k] = v[*startFrom:]
+	}
+
+	// Print the patterns.
+	pr := pattern.NewPrint(trimmedVoices)
+	pr.Print(pattern.PatternPosition)
+
+	err = pattern.Play(trimmedVoices)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func baseNote() pattern.Note {
+	return pattern.Note{
+		Note:     midicom.Note(cycles.C4),
+		Duration: 200,
+		Velocity: 100,
+	}
+}
+
+func compo(allVoices map[int][]pattern.Pattern, p midicom.MidiCom) {
 	// BD
 	pat := pattern.Pattern{
 		Midicom: p,
-		Notes:   []pattern.Note{note},
+		Notes:   []pattern.Note{baseNote()},
 		Channel: midicom.Channel(cycles.T1),
 		Meta: pattern.Meta{
 			Synth: string(cycles.CYCLES),
@@ -39,14 +68,16 @@ func main() {
 		},
 	}
 
+	pat.Notes[0].Duration = 50
 	pat.Notes[0].CC = cycles.PT1()
+	pat.Notes[0].CC[cycles.VOLUMEDIST] = int8(80)
 
 	allVoices[0] = append(allVoices[0], pat)
 
 	// SN
 	pat = pattern.Pattern{
 		Midicom: p,
-		Notes:   []pattern.Note{note},
+		Notes:   []pattern.Note{baseNote()},
 		Channel: midicom.Channel(cycles.T2),
 		Meta: pattern.Meta{
 			Synth: pat.Synth,
@@ -55,13 +86,14 @@ func main() {
 	}
 
 	pat.Notes[0].CC = cycles.PT2()
+	pat.Notes[0].CC[cycles.DELAY] = int8(120)
 
 	allVoices[0] = append(allVoices[0], pat)
 
 	// Metal
 	pat = pattern.Pattern{
 		Midicom: p,
-		Notes:   []pattern.Note{note},
+		Notes:   []pattern.Note{baseNote()},
 		Channel: midicom.Channel(cycles.T3),
 		Meta: pattern.Meta{
 			Synth: pat.Synth,
@@ -76,7 +108,7 @@ func main() {
 	// Perc
 	pat = pattern.Pattern{
 		Midicom: p,
-		Notes:   []pattern.Note{note},
+		Notes:   []pattern.Note{baseNote()},
 		Channel: midicom.Channel(cycles.T4),
 		Meta: pattern.Meta{
 			Synth: pat.Synth,
@@ -91,7 +123,7 @@ func main() {
 	// Tone
 	pat = pattern.Pattern{
 		Midicom: p,
-		Notes:   []pattern.Note{note},
+		Notes:   []pattern.Note{baseNote()},
 		Channel: midicom.Channel(cycles.T5),
 		Meta: pattern.Meta{
 			Synth: pat.Synth,
@@ -99,14 +131,21 @@ func main() {
 		},
 	}
 
+	pat.Notes[0].Note = midicom.Note(cycles.C2)
+	pat.Notes[0].Duration = 3000
+
 	pat.Notes[0].CC = cycles.PT5()
+	pat.Notes[0].CC[cycles.REVERB] = int8(100)
+	pat.Notes[0].CC[cycles.COLOR] = int8(50)
+	pat.Notes[0].CC[cycles.SHAPE] = int8(40)
+	pat.Notes[0].CC[cycles.DECAY] = int8(70)
 
 	allVoices[0] = append(allVoices[0], pat)
 
 	// Chord
 	for o := range 37 {
-		chordNote := note
-		chordNote.Note = midicom.Note(int(note.Note-5) + o)
+		chordNote := baseNote()
+		chordNote.Note = midicom.Note(int(chordNote.Note-10) + o)
 		pat = pattern.Pattern{
 			Midicom: p,
 			Notes:   []pattern.Note{chordNote},
@@ -117,27 +156,22 @@ func main() {
 			},
 		}
 
+		if o == 0 {
+			pat.Notes[0].Velocity = 0
+		}
+
 		pat.Notes[0].Duration = pat.Notes[0].Duration + float64(o*10)
 		pat.Notes[0].CC = cycles.PT6()
 		pat.Notes[0].CC[cycles.SWEEP] = int8(o * 2)
 		pat.Notes[0].CC[cycles.CONTOUR] = int8(o * 2)
 		pat.Notes[0].CC[cycles.REVERB] = int8(o * 2)
 		pat.Notes[0].CC[cycles.COLOR] = int8(o * 2)
-		pat.Notes[0].CC[cycles.DECAY] = int8(o * 2)
+		pat.Notes[0].CC[cycles.DECAY] = int8(o*2) + 10
 		pat.Notes[0].CC[cycles.DELAY] = int8(o)
 		pat.Notes[0].CC[cycles.REVERBSIZE] = int8(o * 2)
 		pat.Notes[0].CC[cycles.REVERBTONE] = int8(o * 2)
 		pat.Notes[0].CC[cycles.SHAPE] = int8(cycles.Unisonx2) + int8(o)
 
 		allVoices[0] = append(allVoices[0], pat)
-	}
-
-	// 	Print
-	pr := pattern.NewPrint(allVoices)
-	pr.Print(pattern.PatternPosition)
-
-	err = pattern.Play(allVoices)
-	if err != nil {
-		panic(err)
 	}
 }
